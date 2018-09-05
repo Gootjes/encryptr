16517e816567228d9511a4c570d4a30c9207c7dc
gynt <gynt@users.noreply.github.com>
2018-09-05 13:18:30 +0200

    Introduced requirement to re-enter password when encrypting

--- R/main.R
+++ R/main.R
@@ -13,7 +13,7 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-askPassword <- function(password = NULL) {
+askPassword <- function(password = NULL, question = "Please enter password") {
   if(!is.null(password)) {
     if(nchar(password) > 0) {
       return(password)
@@ -24,16 +24,16 @@ askPassword <- function(password = NULL) {
     if(!requireNamespace("getPass", quietly = TRUE)) {
       stop("Package \"getPass\" is needed for asking for passwords when R is not in interactive mode.")
     } else {
-      password <- getPass::getPass("Please enter your password", noblank = T)
+      password <- getPass::getPass(question, noblank = T)
       return(password)
     }
   }
 
   if(is.null(password)) {
-    return(openssl::askpass())
+    return(openssl::askpass(prompt = question))
   }
   if(nchar(password) == 0) {
-    return(openssl::askpass())
+    return(openssl::askpass(prompt = question))
   }
 
   stop("Cannot set password")
@@ -59,17 +59,66 @@ load <- function(file, envir = parent.frame(), password = NULL) {
   bytes <- unserialize(conn)
   on.exit({close(conn)})
 
-  password <- askPassword(password)
 
-  decrypted_bytes <- tryCatch(unserialize(openssl::aes_cbc_decrypt(bytes, openssl::sha256(charToRaw(password)), iv = attr(bytes,"iv"))), error = function(e){
-    if(e$message == "OpenSSL error in EVP_DecryptFinal_ex: bad decrypt") {
-      stop("Incorrect password", call.=F)
-    } else if(e$message == "OpenSSL error in win32_load: could not load the shared library") {
-      stop("Could not load shared library (openSSL). Wrong password?", call.=F)
+
+  tryDecrypt <- function(bytes, password = NULL, first = T) {
+    result <- list()
+
+    if(!first) {
+      password <- askPassword(password, question = "Something went wrong, please re-enter password")
     } else {
-      stop(e)
+      password <- askPassword(password)
     }
-  })
+
+    if(length(password) == 0) {
+      stop("No password provided.", call. = F)
+    }
+
+    raw_password <- charToRaw(password)
+
+    hashed_password <- openssl::sha256(raw_password)
+
+    iv <- attr(bytes,"iv")
+
+    result <- tryCatch(list(result = 0, data = openssl::aes_cbc_decrypt(bytes, hashed_password, iv = iv)),
+                                error = function(e) {
+                                  if(e$message == "OpenSSL error in EVP_DecryptFinal_ex: bad decrypt") {
+                                    cat("Incorrect password?\n")
+                                    return(list(result = 1))
+                                  } else if(e$message == "OpenSSL error in win32_load: could not load the shared library") {
+                                    #cat("Could not load shared library (openSSL). Wrong password?\n")
+                                    cat("Incorrect password?\n")
+                                    return(list(result = 2))
+                                  } else {
+                                    warning(e)
+                                    return(list(result = 3))
+                                  }
+                                }
+                                )
+
+    return(result)
+
+  }
+
+  tries <- 3
+
+  while(tries > 0) {
+    result <- tryDecrypt(bytes = bytes, password = password, first = (tries == 3))
+
+    if(result$result != 0) {
+      tries <- tries - 1
+      password <- NULL
+    } else {
+      break
+    }
+
+  }
+
+  if(result$result != 0) {
+    stop("Failed to decrypt file")
+  }
+
+  decrypted_bytes <- unserialize(result$data)
 
   sapply(names(decrypted_bytes), function(name){
     envir[[name]] = decrypted_bytes[[name]]
@@ -123,7 +172,19 @@ save <- function(..., list = character(),
 
   bytes <- serialize(data, NULL)
 
-  password <- askPassword(password)
+  getPassword <- function(password) {
+
+    password1 <- askPassword(password)
+    password2 <- askPassword(password, question = "Please re-enter password:")
+
+    if(password1 != password2) {
+      stop("Passwords do not match", call. = F)
+    }
+
+    return(password1)
+  }
+
+  password <- getPassword(password)
 
   iv <- openssl::rand_bytes(16)
   encrypted_bytes <- openssl::aes_cbc_encrypt(bytes, openssl::sha256(charToRaw(password)), iv)
